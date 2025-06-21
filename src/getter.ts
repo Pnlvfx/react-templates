911772ab90b568ae1f9a2a2000687e8fe0a232f6
simone <simonegauli@gmail.com>
2025-06-21 14:01:18 +0200

    add react-native example

--- src/getter.ts
+++ src/getter.ts
@@ -1,13 +1,46 @@
+/* eslint-disable no-console */
 import { execAsync } from '@goatjs/node/exec';
 import { rimraf } from '@goatjs/rimraf';
 import fs from 'node:fs/promises';
+import path from 'node:path';
 
 const root = '.template';
 
 const nextJsTemplate = async () => {
-  await execAsync('npx create-next-app@canary empty --empty --yes --skip-install --no-tailwind', { cwd: root });
+  console.log('Generating nextjs app');
+  const nextJsRoot = path.join(root, 'nextjs');
+  await fs.mkdir(nextJsRoot);
+  await execAsync('npx create-next-app@canary empty --empty --yes --skip-install --no-tailwind', { cwd: nextJsRoot });
+};
+
+// npx create-react-native-library@latest --help for the list of options
+
+const fabric = {
+  slug: 'react-native-fabric',
+  description: '',
+  'author-name': 'simonegauli',
+  'author-email': 'simonegauli@gmail.com',
+  'author-url': 'https://github.com/Pnlvfx',
+  languages: 'kotlin-objc',
+  type: 'fabric-view',
+  'react-native-version': '0.80',
+  example: 'vanilla',
+  interactive: 'false',
+};
+
+const reactNativeBuilderBobFabric = async () => {
+  console.log('Generating react native fabric library');
+  const libraryRoot = path.join(root, 'react-native');
+  await fs.mkdir(libraryRoot);
+  const params = [];
+  for (const [key, value] of Object.entries(fabric)) {
+    params.push(`--${key}`, value);
+  }
+  console.log(params.join(' '));
+  await execAsync(`npx create-react-native-library@latest fabric ${params.join(' ')}`);
 };
 
 await rimraf(root);
 await fs.mkdir(root);
 await nextJsTemplate();
+await reactNativeBuilderBobFabric();
