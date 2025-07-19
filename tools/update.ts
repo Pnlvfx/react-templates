d2d679e3efcb19f86752a53e36dc862142fdba10
simone <simonegauli@gmail.com>
2025-07-19 18:27:35 +0200

    feat(create-turbo): apply official-starter transform

--- tools/update.ts
+++
@@ -1,9 +0,0 @@
-import { updateLocalDeps } from '@goatjs/updater';
-
-await updateLocalDeps({
-  '@goatjs/eslint': 'github:Pnlvfx/eslint',
-  '@goatjs/node': 'github:Pnlvfx/goatjs#workspace=@goatjs/node',
-  '@goatjs/rimraf': 'github:Pnlvfx/goatjs#workspace=@goatjs/rimraf',
-  '@goatjs/typescript-config': 'github:Pnlvfx/typescript-config',
-  '@goatjs/updater': 'github:Pnlvfx/goatjs#workspace=@goatjs/updater',
-});
