d6b3413805502c1427bbdd0b7673f4c65801489a
Simone Gauli <simonegauli@gmail.com>
2025-10-14 20:15:25 +0200

    ignore

--- src/compiler.ts
+++ src/compiler.ts
@@ -3,8 +3,10 @@ import fs from 'node:fs/promises';
 import { templateRoot } from './config.js';
 import { reactNativeApp } from './react-native.js';
 import { nextJsTemplate } from './next.js';
-import { turboTemplate } from './turbo.js';
 import { reactNativeBuilderBob } from './bob.js';
+import { checkGitStatus } from '@goatjs/node/dev/git';
+
+await checkGitStatus();
 
 // TODO use kebabCase from goatjs node to parse the keys from camel (change key to camel)
 // add check for left changes on git
@@ -26,5 +28,5 @@ await reactNativeBuilderBob('fabric', {
 });
 await reactNativeApp('example', { 'install-pods': false, 'skip-git-init': true, 'skip-install': true });
 
-// TURBO SUCK, THERE I S NO WAY TO PREVENT COMMITTING AFTER RUNNINNG THIS SHIT.
+// TURBO SUCK, THERE IS NO WAY TO PREVENT COMMITTING AFTER RUNNINNG THIS SHIT.
 // await turboTemplate('example', { example: 'with-tailwind', 'package-manager': 'yarn', 'skip-install': true });
