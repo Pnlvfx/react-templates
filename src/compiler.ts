901d71b4c431c715ca02c2eb0f56e471b77c285b
Simone Gauli <simonegauli@gmail.com>
2025-10-14 21:52:36 +0200

    add vite template

--- src/compiler.ts
+++ src/compiler.ts
@@ -4,6 +4,9 @@ import { reactNativeApp } from './react-native.js';
 import { nextJsTemplate } from './next.js';
 import { reactNativeBuilderBob } from './bob.js';
 import { checkGitStatus } from '@goatjs/node/dev/git';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import { reactViteTemplate } from './vite.js';
 
 await checkGitStatus();
 
@@ -14,20 +17,63 @@ try {
   await fs.mkdir(templateRoot);
 } catch {}
 
-await nextJsTemplate('example', { empty: true, noTailwind: true, skipInstall: true });
-await reactNativeBuilderBob('fabric', {
-  slug: 'react-native-fabric',
-  description: 'fabric example',
-  'author-name': 'simonegauli',
-  'author-email': 'simonegauli@gmail.com',
-  'author-url': 'https://github.com/Pnlvfx',
-  'repo-url': 'https://github.com/Pnlvfx/react-native-fabric',
-  languages: 'kotlin-objc',
-  type: 'fabric-view',
-  example: 'vanilla',
-  interactive: 'false',
-});
-await reactNativeApp('example', { 'install-pods': false, 'skip-git-init': true, 'skip-install': true });
+const { framework } = await yargs(hideBin(process.argv))
+  .version(false)
+  .strict()
+  .help()
+  .option('framework', { alias: 'f', type: 'string' })
+  .parseAsync();
+
+const createNextApp = () => {
+  return nextJsTemplate('example', { empty: true, noTailwind: true, skipInstall: true });
+};
+
+const createReactViteApp = () => {
+  return reactViteTemplate('example');
+};
+
+const createFabricLibrary = () => {
+  return reactNativeBuilderBob('fabric', {
+    slug: 'react-native-fabric',
+    description: 'fabric example',
+    'author-name': 'simonegauli',
+    'author-email': 'simonegauli@gmail.com',
+    'author-url': 'https://github.com/Pnlvfx',
+    'repo-url': 'https://github.com/Pnlvfx/react-native-fabric',
+    languages: 'kotlin-objc',
+    type: 'fabric-view',
+    example: 'vanilla',
+    interactive: 'false',
+  });
+};
+
+const createReactNativeApp = () => {
+  return reactNativeApp('example', { 'install-pods': false, 'skip-git-init': true, 'skip-install': true });
+};
+
+switch (framework) {
+  case 'nextjs': {
+    await createNextApp();
+    break;
+  }
+  case 'react-vite': {
+    await createReactViteApp();
+    break;
+  }
+  case 'react-native-library': {
+    await createFabricLibrary();
+    break;
+  }
+  case 'react-native-app': {
+    await createReactNativeApp();
+    break;
+  }
+}
+
+await createNextApp();
+await createReactViteApp();
+await createFabricLibrary();
+await createReactNativeApp();
 
 // TURBO SUCK, THERE IS NO WAY TO PREVENT COMMITTING AFTER RUNNINNG THIS SHIT.
 // await turboTemplate('example', { example: 'with-tailwind', 'package-manager': 'yarn', 'skip-install': true });
---
+++ src/vite.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import { execAsync } from '@goatjs/node/exec';
+import { templateRoot } from './config.js';
+import { rimraf } from '@goatjs/rimraf';
+
+export const reactViteTemplate = async (name: string) => {
+  const cwd = path.join(templateRoot, 'react-vite');
+  await rimraf(cwd);
+  await fs.mkdir(cwd);
+  const command = `yarn create vite ${name} --template react-ts`;
+  console.log('Generation react vite app', name, `with command:\n${command}`);
+  await execAsync(command);
+};
