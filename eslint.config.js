e332149d0220f33488bbfcc4a04bfb44ec61aa0c
Simone Gauli <simonegauli@gmail.com>
2025-10-20 02:19:05 +0200

    ignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import { nodeConfigs } from '@goatjs/node-eslint';
 import { defineConfig } from '@eslint/config-helpers';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export default defineConfig([{ ignores: ['dist', '.yarn', '.template'] }, ...nodeConfigs({ tsconfigRootDir: import.meta.dirname })]);
