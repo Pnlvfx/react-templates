1a3db0d0aeabb98bf20f62b087fb826514bc28bb
Simone Gauli <simonegauli@gmail.com>
2026-09-03 19:43:07 +0200

    fix(electron): o

--- src/electron-vite.ts
+++ src/electron-vite.ts
@@ -4,8 +4,8 @@ import path from 'node:path';
 import fs from 'node:fs/promises';
 import { execAsync } from '@goatjs/node/exec';
 import { rimraf } from '@goatjs/rimraf';
-import { templateRoot } from './config.js';
-import { parseBashOptions } from './helpers/bash.js';
+import { templateRoot } from './config.ts';
+import { parseBashOptions } from './helpers/bash.ts';
 
 interface Options {
   template?: 'react' | 'vue' | 'vanilla' | 'svelte';
@@ -16,8 +16,7 @@ export const createElectronApp = async (name: string, { skipInstall, template = 
   const cwd = path.join(templateRoot, 'electron-vite');
   await rimraf(cwd);
   await fs.mkdir(cwd, { recursive: true });
-  /** @ts-expect-error ma porco dio. */
-  const command = `pnpm create electron-vite ${name} --template ${template} ${parseBashOptions(skipInstall)} --yes`;
+  const command = `pnpm create electron-vite ${name} --template ${template}${parseBashOptions({ skip: skipInstall })}`;
   console.log('Generating electron-vite app', name, `with command:\n${command}`);
   await execAsync(command, { cwd });
 };
